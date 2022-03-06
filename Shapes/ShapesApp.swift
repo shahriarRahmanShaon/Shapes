56582945b76b227bf27a2fc1646d2fe528990dcf
shahriarRahmanShaon <shaon463@gmail.com>
2022-03-06 08:05:53 +0600

    Initial Commit

---
+++ Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by sergio shaon on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
